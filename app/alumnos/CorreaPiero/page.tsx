f56334a50ac90279847f443ee2fadd1329398f39
PieroC23 <pierosebastian230305@gmail.com>
2024-11-24 22:14:49 -0500

    Segundo commit

---
+++ app/alumnos/CorreaPiero/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <div className="text-white">
+            <h1>Holaa</h1>
+            <p>parrafoo</p>
+            <section>Esta es la primera seccion</section>
+        </div>
+    )
+}
