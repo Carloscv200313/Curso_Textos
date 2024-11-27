35ff80e85112ce4df4ed418923400c268e878bc6
CalderonMickel <2313110236@untels.edu.pe>
2024-11-26 20:29:52 -0500

    Primer commit

--- app/alumnos/Calderon-Guevara/page.tsx
+++ app/alumnos/Calderon-Guevara/page.tsx
@@ -3,27 +3,32 @@ import Image from "next/image";
 import { motion } from "framer-motion"; // Importa useInView de Framer Motion
 import { InfiniteMovingCardsDemo } from "@/components/infinito";
 import { TypewriterEffect } from "@/components/ui/typewriter-effect";
+import { text } from "stream/consumers";
 export default function Home() {
     const words = [
-        {
-            text: "Ingeniero",
-        },
-        {
-            text: "de",
-            
-        },
-        {
-            text: "Sistemas",
-            
-        },
-        {
-            text: "Developer",
-            className: "text-blue-500 dark:text-indigo-500",
-        },
-        {
-            text: "Web",
-            className: "text-blue-500 dark:text-indigo-500",
-        },
+        {text: "De",},
+
+        {text: " lo",},
+
+        {text: " personal",},
+        
+        {text: " a",},
+        
+        {text: " lo"},
+        
+        { text: "digital:" },
+        
+        {   text:"Explorando" ,
+            className: "text-blue-300 dark:text-blue-400",},
+
+        {text: "mi",
+            className: "text-blue-300 dark:text-blue-400",},
+
+        {text: "nuevo",
+            className: "text-blue-300 dark:text-blue-400",},
+
+        {text: "mundo",
+            className: "text-blue-300 dark:text-blue-400",},
     ];
     return (
         <>
@@ -32,7 +37,7 @@ export default function Home() {
                 <Image
                     width={10000}
                     height={500}
-                    src="/alumnos/carlos_calderon/fondo.jpg"
+                    src="/alumnos/Miguel_Calderon/fondo.jpeg"
                     alt="foto de fondo"
                     className="absolute inset-0 object-cover w-full h-full"
                     style={{ filter: "brightness(0.1)" }} // Oscurece la imagen
@@ -40,7 +45,7 @@ export default function Home() {
                 {/* Texto en la parte superior */}
                 <div className="absolute inset-0 flex items-center justify-center text-white z-10 w-full">
                     <div className="w-2/3">
-                    <div className="flex flex-col items-start justify-start h-[15rem] md:h-[8rem]">
+                        <div className="flex flex-col items-start justify-start h-[15rem] md:h-[8rem]">
                             <TypewriterEffect words={words} />
                         </div>
                     </div>
@@ -51,13 +56,13 @@ export default function Home() {
                     <motion.p
                         initial={{ opacity: 0, x: -100 }}
                         whileInView={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.8 }}
+                        transition={{ duration: 1 }}
                         className="text-3xl md:text-4xl font-extrabold text-gray-300 xl:m-8 xl:text-5xl pb-5"
                     >
                         <Image
                             width={500}
                             height={1000}
-                            src="/alumnos/carlos_calderon/foto.png"
+                            src="/alumnos/Miguel_Calderon/miguel-calderon.jpeg"
                             alt="foto de fondo"
                             className="rounded-3xl shadow-lg shadow-slate-950 h-5/6 w-4/6" // Clases para bordes redondos y sombra
                         />
@@ -70,7 +75,7 @@ export default function Home() {
                         transition={{ duration: 0.5 }}
                         className="text-3xl md:text-4xl font-extrabold text-gray-300 xl:m-8 xl:text-5xl pb-5"
                     >
-                        <span className="border-b-2 border-yellow-800">Sobre Mi</span>
+                        <span className="border-b-2 border-rose-300">Acerca de mi</span>
                     </motion.p>
                     <motion.p
                         initial={{ opacity: 0, y: 50 }}
@@ -78,7 +83,7 @@ export default function Home() {
                         transition={{ duration: 0.5 }}
                         className="mb-4 text-gray-300 text-base md:text-lg lg:text-xl"
                     >
-                        Hola, soy Carlos Calderón, estudiante de Ingeniería de Sistemas en el cuarto ciclo de mi carrera y un apasionado programador de páginas web con un enfoque Full Stack. Tengo experiencia en el desarrollo frontend utilizando HTML, CSS y JavaScript, así como en frameworks como React. En el backend, trabajo con Node.js y Express, y manejo bases de datos como MongoDB y MySQL. Además, estoy familiarizado con herramientas de control de versiones como Git. Me motiva crear soluciones completas que mejoren la experiencia del usuario, y busco constantemente aprender y aplicar nuevas tecnologías en mis proyectos. Estoy emocionado por las oportunidades que se presenten para seguir creciendo en este campo.
+                        Hola muy buenas, soy Miguel Angel Calderon Guevara,soy estudiante de Ingeniería de Sistemas en el cuarto ciclo de la UNTELS "Universidad prestigiosa de Lima xd",soy alguien muy apasionado a la hora de poder crear cosas interesantes y nuevas que me enseña mi carrera,sea sobre páginas webs,bases de datos, nuevos algoritmos, entre otros entornos de ello. Me motiva crear soluciones nuevas que mejoren la experiencia del usuario, y al mismo tiempo aprender y aplicar nuevas tecnologías en mis proyectos. Tengo una emocion grande por seguir en este campo, y poder emprender las oportunidades que se presenten para seguir creciendo en este campo.
                     </motion.p>
                     {/* Aseguramos que los botones están fuera de los párrafos */}
                     <motion.div
@@ -88,7 +93,7 @@ export default function Home() {
                         className="grid gap-4 md:gap-20 grid-cols-1 md:grid-cols-2 mt-6"
                     >
                         <button className="rounded-3xl bg-yellow-500 text-gray-800 px-4 py-2 hover:bg-yellow-400">Contáctame</button>
-                        <button className="rounded-3xl border border-yellow-500 text-yellow-500 px-4 py-2 hover:bg-yellow-500 hover:text-white">Download CV</button>
+
                     </motion.div>
                 </div>
             </div>
@@ -100,14 +105,14 @@ export default function Home() {
                 </p>
                 <div className="grid gap-4 md:gap-10 grid-cols-1 md:grid-cols-2">
                     {[
-                        { skill: "React", percentage: 80 },
-                        { skill: "JavaScript", percentage: 90 },
-                        { skill: "Node Js", percentage: 70 },
-                        { skill: "TypeScript", percentage: 60 },
-                        { skill: "Tailwind", percentage: 80 },
-                        { skill: "Express", percentage: 70 },
-                        { skill: "SQL Server", percentage: 75 },
-                        { skill: "Git", percentage: 90 },
+                        { skill: "Solidaridad", percentage: 95 },
+                        { skill: "Tolerancia", percentage: 80 },
+                        { skill: "Asistencia", percentage: 70 },
+                        { skill: "Lenguajes de programacion", percentage: 30 },
+                        { skill: "Configuracion de Computadoras", percentage: 55 },
+                        { skill: "Programando", percentage: 50 },
+                        { skill: "Liderazgo", percentage: 75 },
+                        { skill: "Motivacion", percentage: 85 },
                     ].map(({ skill, percentage }) => (
                         <div key={skill} className="mb-4">
                             <div className="flex justify-between">
